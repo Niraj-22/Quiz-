f5925153fba13791c954fc513324779ce97a431a
Niraj Rajesh Chordia <110300158+Niraj-22@users.noreply.github.com>
2023-02-07 12:24:26 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following can read and render HTML web pages?",
+    answer: "Web Browser",
+    options: [
+      "Server",
+      "Web Browser",
+      "Empty",
+      "Head Task"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Why were cookies designed?",
+    answer: "For server side",
+    options: [
+      "For no one",
+      "For fun",
+      "For client side",
+      "For server side"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Identify the incorrect HTML tag among the following?",
+    answer: "list",
+    options: [
+      "list",
+      "head",
+      "title",
+      "body"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Identify the empty or void element in HTML?",
+    answer: "br",
+    options: [
+      "sup",
+      "p",
+      "b",
+      "br"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A website is a _______  cookie.?",
+    answer: "Transient",
+    options: [
+      "Transient",
+      "Volatile",
+      "Non Transient",
+      "Non Volatile"
+    ]
+  },
+
+];
